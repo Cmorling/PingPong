18e8b89c9306e80e5d73e6e0f7414b531c0631e1
cmorling <caspian.morling@gmail.com>
2024-12-01 20:53:42 +0100

    client/networking: removed define ip addr

--- client/networking.h
+++ client/networking.h
@@ -5,8 +5,6 @@
 
 #define DATA_SIZE 400
 #define TIMEOUT_SEC 2
-#define SRC_ADDR "172.20.10.6" //TODO Remove for development
-#define DST_ADDR "172.20.10.4"
 #define INTERFACE "eth0"
 
 int listen_icmp(struct Configuration *c, size_t packet_size, size_t pp_size,void * recv_pp);
